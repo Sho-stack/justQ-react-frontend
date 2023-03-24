b0733a77a1c16a37daacd436648fb99290c49f8c
michalszostak <michal.szostak56@gmail.com>
2023-03-24 16:42:14 +0000

    styling added to questions

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import moment from 'moment';
 
 function Question(props) {
+    const question = props.question;
+    const timeElapsed = moment(question.timestamp).fromNow();
     return (
         <Card className={`mb-3 ${props.theme === "light" ? "light-theme" : "dark-theme"}`}>
-            {console.log('card re-rendered')}
-
+            <Card.Header>
+               <Card.Title>{question.content}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{question.author} - {timeElapsed}</Card.Subtitle>
+            </Card.Header>
             <Card.Body>
-                <Card.Text>{props.question.content}</Card.Text>
+                <small>no replies yet...</small>
             </Card.Body>
+            <Card.Footer>
+            <Card.Text>{question.net_votes} points</Card.Text>
+            </Card.Footer>
+            
         </Card>
     );
 }
